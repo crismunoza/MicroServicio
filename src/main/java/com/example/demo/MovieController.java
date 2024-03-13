ea953f9c4767aa1d357c225442a88f54a6cc39a2
Christian Muñoz Ayala <chri.munoz@duocuc.cl>
2024-03-13 13:20:31 -0300

    agregar 6 pelicuas segun req

--- src/main/java/com/example/demo/MovieController.java
+++ src/main/java/com/example/demo/MovieController.java
@@ -12,7 +12,12 @@ public class MovieController {
     //CREANDO EL CONSTRUCTOR PARA AGREGAR LAS PELICULAS
     public MovieController() {
         movies.add(new Movie(1, "Sueño de Fuga", "1994", "Drama", "Frank Darabont", "Dos hombres encarcelados"));
-        movies.add(new Movie(2, "El Padrino", "1972", "Crimen", "Francis Ford Coppola", "El patriarca envejecido de una dinastía del crimen organizado transfiere el control de su imperio clandestino a su hijo reacio."));        
+        movies.add(new Movie(2, "El Padrino", "1972", "Crimen", "Francis Ford Coppola", "El patriarca envejecido de una dinastía del crimen organizado transfiere el control de su imperio clandestino a su hijo reacio.")); 
+        movies.add(new Movie(3, "El Padrino II", "1974", "Crimen", "Francis Ford Coppola", "Secuela del Padrino"));
+        movies.add(new Movie(4, "El Padrino III", "1990", "Crimen", "Francis Ford Coppola", "Tercera parte de la trilogía del Padrino"));
+        movies.add(new Movie(5, "Pulp Fiction", "1994", "Drama/Crimen", "Quentin Tarantino", "Historias interrelacionadas en el mundo del crimen de Los Ángeles"));
+        movies.add(new Movie(6, "Forrest Gump", "1994", "Drama", "Robert Zemeckis", "Historia de vida de un hombre con discapacidad intelectual"));
+       
     }
     @GetMapping("/movie")
     public List<Movie> getMovie() {
